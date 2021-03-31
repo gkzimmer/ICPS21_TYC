0f8475dae882cc2b4e55b173b5396316aa639268
gkzimmer <gzimmerman2@gmail.com>
2021-03-30 22:27:25 -0500

    This is the fifth commit, where I am adding a test standard for a simple range calculation function.

--- tests/testing_test_driven_code.py
+++ tests/testing_test_driven_code.py
@@ -4,6 +4,8 @@
 #number averages
 
 import unittest
+import random
+import numpy as np
 from testing_code import test_driven_code
 
 class TestAdd(unittest.TestCase):
@@ -19,4 +21,10 @@ class TestAdd(unittest.TestCase):
     def test_find_avg_whole(self):
         expected = 5
         result = test_driven_code.find_avg(1,0,0,10,10,10)
+        self.assertEqual(expected,result)
+
+    #Now, let's test another function, this time finding the range of values
+    def test_range(self):
+        expected = 100
+        result = test_drive_code.find_range(0,10,20,30,40,100)
         self.assertEqual(expected,result)
